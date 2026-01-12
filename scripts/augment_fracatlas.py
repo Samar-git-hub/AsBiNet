b80edff43d1247025e2c34298cffe10f9336c413
Samar <samarkamat88@gmail.com>
2026-01-12 23:26:18 +0530

    augmentation/resizing pipeline completed

--- scripts/augment_fracatlas.py
+++ scripts/augment_fracatlas.py
@@ -22,7 +22,12 @@ aug_pipeline = A.Compose([
 
     # Resize preserving aspect ratio
     A.LongestMaxSize(max_size=target_size),
-    A.PadIfNeeded(min_height=target_size, min_width=target_size, border_mode=cv2.BORDER_CONSTANT, value=0, max_value=0)
+    A.PadIfNeeded(min_height=target_size, min_width=target_size, border_mode=cv2.BORDER_CONSTANT, value=0, mask_value=0)
+])
+
+resize_pipeline = A.Compose([
+    A.LongestMaxSize(max_size=target_size),
+    A.PadIfNeeded(min_height=target_size, min_width=target_size, border_mode=cv2.BORDER_CONSTANT, value=0, mask_value=0)
 ])
 
 def load_coco_data(root_dir):
@@ -48,4 +53,99 @@ def load_coco_data(root_dir):
 
     return img_id_map, ann_map
         
+def create_dataset(split_name, mode='original_resized'):
+    # 'augmented' for 50 data augmentation iterations for each image in the train split
+    # 'original_resized' fo resizing the train, validation and test splits
     
+    print(f"Processing {split_name} ({mode})")
+
+    csv_path = os.path.join(root_dir, 'Utilities', 'Fracture Split', f"{split_name}.csv")
+    if not os.path.exists(csv_path):
+        print("CSV not found")
+        return
+    
+    df = pd.read_csv(csv_path)
+    id_to_file, id_to_anns = load_coco_data(root_dir)
+
+    if mode == 'augmented':
+        base_out = aug_output_dir
+        current_multiplier = multiplier
+        pipeline = aug_pipeline
+    else:
+        base_out = orig_resized_dir
+        current_multiplier = 1
+        pipeline = resize_pipeline
+
+    split_out_dir = os.path.join(base_out, split_name)
+    img_out_dir = os.path.join(split_out_dir, 'images')
+    mask_out_dir = os.path.join(split_out_dir, 'masks')
+    os.makedirs(img_out_dir, exist_ok=True)
+    os.makedirs(mask_out_dir, exist_ok=True)
+
+    new_csv_rows = []
+
+    for index, row in tqdm(df.iterrows(), total=len(df)):
+        filename = row['image_id']
+
+        img_id_numeric = None
+        for k, v in id_to_file.items():
+            if v == filename:
+                img_id_numeric = k
+                break
+        
+        if img_id_numeric is None: 
+            continue
+
+        img_path = os.path.join(root_dir, 'images', 'Fractured', filename)
+        if not os.path.exists(img_path):
+            continue
+
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        h, w = image.shape[:2]
+        
+        # Creating high-resolution mask
+        mask = np.zeros((h, w), dtype=np.uint8)
+        
+        if img_id_numeric in id_to_anns:
+            for pols in id_to_anns[img_id_numeric]:
+                for poly in pols:
+                    pts = np.array(poly).reshape((-1, 1, 2)).astype(np.int32)
+                    cv2.fillPoly(mask, [pts], 1)
+
+        
+        # Generation
+        for i in range(current_multiplier):
+            transformed = pipeline(image=image, mask=mask)
+            trans_img = transformed['image']
+            trans_mask = transformed['mask']
+
+            if mode == 'augmented':
+                new_filename = f"{os.path.splitext(filename)[0]}_aug_{i}.jpg"
+                new_maskname = f"{os.path.splitext(filename)[0]}_aug_{i}.png"
+            else:
+                new_filename = filename
+                new_maskname = f"{os.path.splitext(filename)[0]}.png"
+            
+            save_img_path = os.path.join(img_out_dir, new_filename)
+            save_mask_path = os.path.join(mask_out_dir, new_maskname)
+
+            # Save 
+            print(f"Saving {mode} images and masks")
+            cv2.imwrite(save_img_path, cv2.cvtColor(trans_img, cv2.COLOR_RGB2BGR))
+            cv2.imwrite(save_mask_path, trans_mask * 255)
+
+            new_csv_rows.append({
+                'image_id': new_filename,
+                'mask_file': new_maskname,
+                'original_source': filename
+            })
+    
+    csv_save_path = os.path.join(base_out, f'{split_name}.csv')
+    
+    pd.DataFrame(new_csv_rows).to_csv(csv_save_path, index=False)
+    print(f"Saved CSV to {csv_save_path}")
+
+if __name__ == "__main__":
+
+    create_dataset('train', mode='augmented')
